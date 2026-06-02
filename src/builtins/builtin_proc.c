0788d635fdcab9e5d4b45eb245e3d859283dfa4e
LESdylan <dev.pro.photo@gmail.com>
2026-06-02 05:05:26 +0200

    feat(builtin): exec (replace process), wait, times

---
+++ src/builtins/builtin_proc.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_proc.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/02 00:00:00 by dlesieur          #+#    #+#             */
+/*   Updated: 2026/06/02 00:00:00 by dlesieur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtins_private.h"
+#include "executor.h"
+#include "env.h"
+#include <errno.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/times.h>
+#include <unistd.h>
+
+/* Build a NULL-terminated argv array from argv[1..] for execve. */
+static char	**dup_exec_argv(t_vec argv)
+{
+	char	**out;
+	size_t	i;
+
+	out = ft_calloc(argv.len, sizeof(char *));
+	if (!out)
+		return (NULL);
+	i = 1;
+	while (i < argv.len)
+	{
+		out[i - 1] = ((char **)argv.ctx)[i];
+		i++;
+	}
+	return (out);
+}
+
+/* exec [command [args]]: replace the shell with command (or, with no command,
+   leave the already-applied redirections in place). */
+int	builtin_exec(t_shell *state, t_vec argv)
+{
+	char	*path;
+	char	**xargv;
+	char	**envp;
+
+	if (argv.len < 2)
+		return (0);
+	if (find_cmd_path(state, ((char **)argv.ctx)[1], &path) != 0)
+	{
+		ft_eprintf("%s: exec: %s: not found\n", state->ctx,
+			((char **)argv.ctx)[1]);
+		exit(127);
+	}
+	xargv = dup_exec_argv(argv);
+	envp = get_envp(state, path);
+	execve(path, xargv, envp);
+	ft_eprintf("%s: exec: %s: %s\n", state->ctx, path, strerror(errno));
+	exit(126);
+}
+
+/* wait [pid]: wait for background children (all of them if no pid given). */
+int	builtin_wait(t_shell *state, t_vec argv)
+{
+	int		status;
+	pid_t	pid;
+
+	status = 0;
+	if (argv.len >= 2)
+	{
+		pid = (pid_t)ft_atoi(((char **)argv.ctx)[1]);
+		if (waitpid(pid, &status, 0) < 0)
+			return (127);
+		if (WIFEXITED(status))
+			return (WEXITSTATUS(status));
+		return (128 + WTERMSIG(status));
+	}
+	while (waitpid(-1, &status, 0) > 0)
+		;
+	(void)state;
+	return (0);
+}
+
+/* times: print accumulated user/system CPU time for the shell and children. */
+int	builtin_times(t_shell *state, t_vec argv)
+{
+	struct tms	t;
+	long		hz;
+
+	(void)state;
+	(void)argv;
+	hz = sysconf(_SC_CLK_TCK);
+	if (hz <= 0)
+		hz = 100;
+	times(&t);
+	ft_printf("%ldm%ld.%03lds %ldm%ld.%03lds\n",
+		(t.tms_utime / hz) / 60, (t.tms_utime / hz) % 60,
+		(t.tms_utime % hz) * 1000 / hz,
+		(t.tms_stime / hz) / 60, (t.tms_stime / hz) % 60,
+		(t.tms_stime % hz) * 1000 / hz);
+	ft_printf("%ldm%ld.%03lds %ldm%ld.%03lds\n",
+		(t.tms_cutime / hz) / 60, (t.tms_cutime / hz) % 60,
+		(t.tms_cutime % hz) * 1000 / hz,
+		(t.tms_cstime / hz) / 60, (t.tms_cstime / hz) % 60,
+		(t.tms_cstime % hz) * 1000 / hz);
+	return (0);
+}
