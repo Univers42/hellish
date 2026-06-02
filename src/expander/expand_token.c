6aa13caff165d8eff06d01e9f535acace75f848d
LESdylan <dev.pro.photo@gmail.com>
2026-06-02 05:05:26 +0200

    feat(params): expand $@/$* (join positionals) and quoted "$@" deferral in expand_token

--- src/expander/expand_token.c
+++ src/expander/expand_token.c
@@ -14,11 +14,50 @@
 
 char	*expand_param_format(t_shell *state, const char *s, int slen);
 
-void	expand_token(t_shell *state, t_token	*curr_tt)
+/* Join the positional parameters $1..$# with the first char of IFS (space by
+   default). Used for $@/$*; quoted "$@" multi-field is a known limitation. */
+char	*join_positionals(t_shell *state)
+{
+	t_string	out;
+	char		*cnt;
+	char		*k;
+	char		*v;
+	int			i;
+
+	cnt = env_expand(state, "#");
+	i = 0;
+	vec_init(&out);
+	out.elem_size = 1;
+	while (cnt && ++i <= ft_atoi(cnt))
+	{
+		k = ft_itoa(i);
+		v = env_expand(state, k);
+		free(k);
+		if (i > 1 && env_get_ifs(&state->env))
+			vec_push(&out, &env_get_ifs(&state->env)[0]);
+		if (v)
+			vec_push_str(&out, v);
+	}
+	return (vec_push(&out, &(char){0}), (char *)out.ctx);
+}
+
+void	expand_token(t_shell *state, t_token *curr_tt, bool split_ctx)
 {
 	char	*temp;
 	char	*fmt;
 
+	if (split_ctx && curr_tt->tt == TT_DQENVVAR && curr_tt->len == 1
+		&& curr_tt->start[0] == '@')
+		return ;
+	if (curr_tt->len == 1 && (curr_tt->start[0] == '@'
+			|| curr_tt->start[0] == '*'))
+	{
+		temp = join_positionals(state);
+		curr_tt->start = temp;
+		curr_tt->len = (int)ft_strlen(temp);
+		curr_tt->allocated = true;
+		return ;
+	}
 	if (curr_tt->len == 0)
 	{
 		if (curr_tt->start && (*curr_tt->start == '\''
