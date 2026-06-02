c2d8ddd27c63fd10dfb34d43dc5980e909204a1b
LESdylan <dev.pro.photo@gmail.com>
2026-06-02 05:05:26 +0200

    feat(history): !!, !n, !-n, !prefix, !?str? and ^old^new^ quick substitution

--- src/infrastructure/history_expand.c
+++ src/infrastructure/history_expand.c
@@ -47,6 +47,27 @@ static char	*hist_search_prefix(t_shell *state, const char *prefix, int len)
 	return (NULL);
 }
 
+/* !?str[?] : most recent history entry containing `str`. */
+static char	*hist_search_contains(t_shell *state, const char *needle, int len)
+{
+	int		i;
+	char	*nd;
+	char	*cmd;
+
+	nd = ft_strndup(needle, len);
+	if (!nd)
+		return (NULL);
+	i = (int)state->hist.hist_cmds.len - 1;
+	while (i >= 0)
+	{
+		cmd = ((char **)state->hist.hist_cmds.ctx)[i];
+		if (ft_strnstr(cmd, nd, ft_strlen(cmd)))
+			return (free(nd), cmd);
+		i--;
+	}
+	return (free(nd), NULL);
+}
+
 static char	*resolve_bang(t_shell *state, const char *s, size_t *adv)
 {
 	int		num;
@@ -55,6 +76,14 @@ static char	*resolve_bang(t_shell *state, const char *s, size_t *adv)
 
 	if (s[0] == '!')
 		return (*adv = 1, hist_last(state));
+	if (s[0] == '?')
+	{
+		wlen = 1;
+		while (s[wlen] && s[wlen] != '?' && s[wlen] != '\n')
+			wlen++;
+		*adv = wlen + (s[wlen] == '?');
+		return (hist_search_contains(state, s + 1, (int)wlen - 1));
+	}
 	if (s[0] == '-' && s[1] >= '0' && s[1] <= '9')
 	{
 		neg = 1;
@@ -96,6 +125,50 @@ static int	in_single_quote(const char *s, size_t pos)
 	return (sq);
 }
 
+/* last command with the first occurrence of `old` replaced by `new`. */
+static char	*replace_first(const char *s, const char *old, const char *nw)
+{
+	t_string	r;
+	char		*occ;
+
+	occ = ft_strnstr(s, old, ft_strlen(s));
+	if (!occ || !*old)
+		return (NULL);
+	vec_init(&r);
+	r.elem_size = 1;
+	vec_push_nstr(&r, (char *)s, occ - s);
+	vec_push_str(&r, (char *)nw);
+	vec_push_str(&r, occ + ft_strlen(old));
+	return (vec_push(&r, &(char){0}), (char *)r.ctx);
+}
+
+/* ^old^new[^] : re-run the last command with old -> new (csh quick sub). */
+static char	*quick_sub(t_shell *state, const char *input)
+{
+	const char	*c2;
+	const char	*c3;
+	char		*old;
+	char		*nw;
+	char		*res;
+
+	c2 = ft_strchr(input + 1, '^');
+	if (!c2 || !hist_last(state))
+		return (NULL);
+	c3 = ft_strchr(c2 + 1, '^');
+	old = ft_strndup(input + 1, c2 - (input + 1));
+	if (c3)
+		nw = ft_strndup(c2 + 1, c3 - (c2 + 1));
+	else
+		nw = ft_strndup(c2 + 1, ft_strcspn(c2 + 1, "\n"));
+	res = replace_first(hist_last(state), old, nw);
+	free(old);
+	free(nw);
+	if (!res)
+		return (ft_eprintf("%s: :s: substitution failed\n", state->ctx),
+			ft_strdup(""));
+	return (ft_printf("%s\n", res), res);
+}
+
 char	*expand_history(t_shell *state, const char *input)
 {
 	t_string	result;
@@ -105,6 +178,8 @@ char	*expand_history(t_shell *state, const char *input)
 
 	if (!state->hist.hist_active || !input)
 		return (NULL);
+	if (input[0] == '^')
+		return (quick_sub(state, input));
 	vec_init(&result);
 	result.elem_size = 1;
 	i = 0;
